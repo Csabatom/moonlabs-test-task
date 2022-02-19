03d3f07968ecfe2815530c571d9b6351dcc66315
Nyírő Csaba <nyirocsabi@gmail.com>
2022-02-19 17:36:37 +0100

    added design

--- src/app/code-upload/codeupload.component.ts
+++ src/app/code-upload/codeupload.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { DatePipe } from '@angular/common'
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-codeupload',
   templateUrl: './codeupload.component.html',
-  styleUrls: ['./codeupload.component.scss']
+  styleUrls: ['./codeupload.component.scss'],
+  providers: [DatePipe]
 })
 export class CodeUploadComponent implements OnInit {
+  dates = this.getDates(new Date(2022, 1, 1), new Date(2022, 3, 1));
+  time = Array.from(Array(60).keys());
+  
+  constructor(readonly datepipe: DatePipe) { }
 
-  constructor() { }
+  ngOnInit(): void { }
 
-  ngOnInit(): void {
+  getDates(startDate: Date, endDate: Date): Date[] {
+    let dateArray: Date[] = [];
+    let currentDate = startDate
+    while(currentDate <= endDate) {
+      dateArray.push(moment(currentDate).toDate())
+      currentDate = moment(currentDate).add(1, 'days').toDate();
+    }
+    return dateArray;
   }
 
+  isToday(date: Date): boolean {
+    return this.datepipe.transform(date, 'yyyy.MM.dd') == this.datepipe.transform(new Date(), 'yyyy.MM.dd');
+  }
 }
