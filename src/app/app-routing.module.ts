9ba76115b6e8d62abaa194825cc4e8e35bb7d41a
Nyírő Csaba <nyirocsabi@gmail.com>
2022-02-19 13:57:59 +0100

    added bootstrap and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CodeUploadComponent } from './code-upload/codeupload.component';
+import { RegistrationComponent } from './registration/registration.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: CodeUploadComponent
+  },
+  {
+    path: 'registration',
+    component: RegistrationComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/code-upload/codeupload.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-codeupload',
+  templateUrl: './codeupload.component.html',
+  styleUrls: ['./codeupload.component.scss']
+})
+export class CodeUploadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
